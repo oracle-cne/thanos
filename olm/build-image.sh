087dfe30d2b542bc3c54d83ed3f14d926d36f5ca
Thomas Tanaka <thomas.tanaka@oracle.com>
2025-09-12 13:22:11 -0700

    trimpath and ldflags
    Signed-off-by: Thomas Tanaka <thomas.tanaka@oracle.com>

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -9,6 +9,7 @@ docker_tag=${registry}/${name}:v${version}
 pwd
 ls -lrt
 patch -p0 < olm/basic_auth.patch
+patch -p0 < olm/go_extra_flags.patch
 
 docker build --pull \
     --build-arg https_proxy=${https_proxy} \
