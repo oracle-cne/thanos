fdf58f11097f39f59cc676510275e1766dc10b8d
Thomas Tanaka <thomas.tanaka@oracle.com>
2025-09-15 09:48:04 -0700

    Template fix
    Signed-off-by: Thomas Tanaka <thomas.tanaka@oracle.com>

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -1,10 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 export GOPATH=$(go env GOPATH)
 
 name="thanos"
-version="{{{ .major }}}.{{{ .minor }}}.{{{ .patch }}}"
+version="0.39.2"
 registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}
+
+pwd
+ls -lrt
+mkdir -p pkg/basicauth
+cp olm/options.go pkg/basicauth/
+patch -p0 < olm/basic_auth.patch
+patch -p0 < olm/go_extra_flags.patch
 
 docker build --pull \
     --build-arg https_proxy=${https_proxy} \
