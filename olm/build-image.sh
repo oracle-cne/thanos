6ec4313ed671e28c880c3608b9753b5302c5060f
Thomas Tanaka <thomas.tanaka@oracle.com>
2025-09-16 09:16:14 -0700

    remove patch
    Signed-off-by: Thomas Tanaka <thomas.tanaka@oracle.com>

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -6,11 +6,6 @@ version="{{{ .major }}}.{{{ .minor }}}.{{{ .patch }}}"
 registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}
 
-pwd
-ls -lrt
-mkdir -p pkg/basicauth
-cp olm/options.go pkg/basicauth/
-patch -p0 < olm/basic_auth.patch
 patch -p0 < olm/go_extra_flags.patch
 
 docker build --pull \
