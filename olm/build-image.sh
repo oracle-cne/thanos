0858917766a36c855c432c0586eafb733db47e89
Thomas Tanaka <thomas.tanaka@oracle.com>
2025-09-16 13:04:43 -0700

    FIX2
    Signed-off-by: Thomas Tanaka <thomas.tanaka@oracle.com>

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -6,8 +6,6 @@ version="0.39.2"
 registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}
 
-cp olm/go-build.sh .
-
 docker build --pull \
     --build-arg https_proxy=${https_proxy} \
     -t ${docker_tag} -f ./olm/builds/Dockerfile .
