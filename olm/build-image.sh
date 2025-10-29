19becd9677bb17bf7c340782fa0967e9eb4759c2
Oracle Cloud Native Environment Authors <noreply@oracle.com>
2025-10-29 08:31:35 +0000

    Updated to v0.40.1 from https://github.com/thanos-io/thanos.git
    Generated from template at 57bc4c9179e50f5edf51b3abd217856e897ebb22

---
+++ olm/build-image.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash -x
+export GOPATH=$(go env GOPATH)
+
+name="thanos"
+version="0.40.1
+registry="container-registry.oracle.com/olcne"
+docker_tag=${registry}/${name}:v${version}
+
+docker build --pull \
+    --build-arg https_proxy=${https_proxy} \
+    -t ${docker_tag} -f ./olm/builds/Dockerfile .
