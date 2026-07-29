05bcd0f725f1cf13e31985815d441092e5e2a0db
Oracle Cloud Native Environment Authors <noreply@oracle.com>
2026-07-29 08:38:22 +0000

    Updated vendor folder

---
+++ vendor/github.com/thanos-io/promql-engine/execution/function/histogram.go
@@ -0,0 +1,368 @@
+// Copyright (c) The Thanos Community Authors.
+// Licensed under the Apache License 2.0.
+
+package function
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"strconv"
+	"sync"
+
+	"github.com/thanos-io/promql-engine/execution/model"
+	"github.com/thanos-io/promql-engine/execution/telemetry"
+	"github.com/thanos-io/promql-engine/extlabels"
+	"github.com/thanos-io/promql-engine/logicalplan"
+	"github.com/thanos-io/promql-engine/query"
+	"github.com/thanos-io/promql-engine/warnings"
+
+	"github.com/cespare/xxhash/v2"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/promql"
+	"github.com/prometheus/prometheus/promql/parser/posrange"
+	"github.com/prometheus/prometheus/util/annotations"
+)
+
+type histogramSeries struct {
+	outputID         int
+	upperBound       float64
+	hasBucketValue   bool
+	bucketLabelValue string // original bucket label value for use in warnings
+}
+
+// histogramOperator is a function operator that calculates percentiles.
+type histogramOperator struct {
+	once   sync.Once
+	series []labels.Labels
+
+	funcName   string
+	funcArgs   logicalplan.Nodes
+	stepsBatch int
+	vectorOp   model.VectorOperator
+	scalar1Op  model.VectorOperator
+	scalar2Op  model.VectorOperator
+
+	// scalarPoints is a reusable buffer for points from the first argument of histogram_quantile.
+	scalar1Points []float64
+	scalar2Points []float64
+
+	// outputIndex is a mapping from input series ID to the output series ID and its upper boundary value
+	// parsed from the le label.
+	// If outputIndex[i] is nil then series[i] has no valid `le` label.
+	outputIndex []*histogramSeries
+
+	// needed to compile warnings on mixed histograms
+	inputSeriesNames []string
+
+	// seriesBuckets are the buckets for each individual conventional histogram series.
+	seriesBuckets []promql.Buckets
+
+	// badBucketWarned tracks which series have already emitted bad bucket label warnings.
+	badBucketWarned map[uint64]bool
+
+	vectorBuf  []model.StepVector
+	scalar1Buf []model.StepVector
+	scalar2Buf []model.StepVector
+}
+
+func newHistogramOperator(
+	call *logicalplan.FunctionCall,
+	nextOps []model.VectorOperator,
+	stepsBatch int,
+	opts *query.Options,
+) model.VectorOperator {
+	o := &histogramOperator{
+		funcName:   call.Func.Name,
+		funcArgs:   call.Args,
+		stepsBatch: stepsBatch,
+	}
+
+	switch o.funcName {
+	case "histogram_quantile":
+		o.scalar1Op = nextOps[0]
+		o.vectorOp = nextOps[1]
+		o.scalar1Points = make([]float64, stepsBatch)
+	case "histogram_fraction":
+		o.scalar1Op = nextOps[0]
+		o.scalar2Op = nextOps[1]
+		o.vectorOp = nextOps[2]
+		o.scalar1Points = make([]float64, stepsBatch)
+		o.scalar2Points = make([]float64, stepsBatch)
+	default:
+		panic("unsupported function passed")
+	}
+	return telemetry.NewOperator(telemetry.NewTelemetry(o, opts), o)
+}
+
+func (o *histogramOperator) String() string {
+	return fmt.Sprintf("[%s](%v)", o.funcName, o.funcArgs)
+}
+
+func (o *histogramOperator) Explain() (next []model.VectorOperator) {
+	switch o.funcName {
+	case "histogram_quantile":
+		return []model.VectorOperator{o.scalar1Op, o.vectorOp}
+	case "histogram_fraction":
+		return []model.VectorOperator{o.scalar1Op, o.scalar2Op, o.vectorOp}
+	}
+	return nil
+}
+
+func (o *histogramOperator) Series(ctx context.Context) ([]labels.Labels, error) {
+	var err error
+	o.once.Do(func() { err = o.loadSeries(ctx) })
+	if err != nil {
+		return nil, err
+	}
+	return o.series, nil
+}
+
+func (o *histogramOperator) Next(ctx context.Context, buf []model.StepVector) (int, error) {
+	select {
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	default:
+	}
+
+	var err error
+	o.once.Do(func() { err = o.loadSeries(ctx) })
+	if err != nil {
+		return 0, err
+	}
+
+	// First get vector data to know how many steps we need
+	vectorN, err := o.vectorOp.Next(ctx, o.vectorBuf)
+	if err != nil {
+		return 0, err
+	}
+	if vectorN == 0 {
+		return 0, nil
+	}
+
+	// Now get scalar data for the same number of steps
+	switch o.funcName {
+	case "histogram_quantile":
+		scalar1N, err := o.scalar1Op.Next(ctx, o.scalar1Buf[:vectorN])
+		if err != nil {
+			return 0, err
+		}
+
+		if scalar1N == 0 {
+			return 0, nil
+		}
+
+		o.scalar1Points = o.scalar1Points[:0]
+		for i := range scalar1N {
+			scalar := o.scalar1Buf[i]
+			if len(scalar.Samples) > 0 {
+				sample := scalar.Samples[0]
+				if math.IsNaN(sample) || sample < 0 || sample > 1 {
+					warnings.AddToContext(annotations.NewInvalidQuantileWarning(sample, posrange.PositionRange{}), ctx)
+				}
+				o.scalar1Points = append(o.scalar1Points, sample)
+			}
+		}
+	case "histogram_fraction":
+		scalar1N, err := o.scalar1Op.Next(ctx, o.scalar1Buf[:vectorN])
+		if err != nil {
+			return 0, err
+		}
+
+		if scalar1N == 0 {
+			return 0, nil
+		}
+
+		o.scalar1Points = o.scalar1Points[:0]
+		for i := range scalar1N {
+			scalar := o.scalar1Buf[i]
+			if len(scalar.Samples) > 0 {
+				sample := scalar.Samples[0]
+				o.scalar1Points = append(o.scalar1Points, sample)
+			}
+		}
+
+		scalar2N, err := o.scalar2Op.Next(ctx, o.scalar2Buf[:vectorN])
+		if err != nil {
+			return 0, err
+		}
+
+		if scalar2N == 0 {
+			return 0, nil
+		}
+
+		o.scalar2Points = o.scalar2Points[:0]
+		for i := range scalar2N {
+			scalar := o.scalar2Buf[i]
+			if len(scalar.Samples) > 0 {
+				sample := scalar.Samples[0]
+				o.scalar2Points = append(o.scalar2Points, sample)
+			}
+		}
+	}
+
+	// Process the vector data and write to output buffer
+	vectors := o.vectorBuf[:vectorN]
+	n, err := o.processInputSeries(ctx, vectors, buf)
+	if err != nil {
+		return 0, err
+	}
+
+	return n, nil
+}
+
+// nolint: unparam
+func (o *histogramOperator) processInputSeries(ctx context.Context, vectors []model.StepVector, buf []model.StepVector) (int, error) {
+	n := 0
+	for stepIndex, vector := range vectors {
+		if n >= len(buf) {
+			break
+		}
+		o.resetBuckets()
+		for i, seriesID := range vector.SampleIDs {
+			outputSeries := o.outputIndex[seriesID]
+			// This means that it has an invalid `le` label.
+			if outputSeries == nil || !outputSeries.hasBucketValue {
+				// Emit warning for invalid bucket label (only once per series).
+				if outputSeries != nil && !o.badBucketWarned[uint64(seriesID)] {
+					o.badBucketWarned[uint64(seriesID)] = true
+					metricName := o.inputSeriesNames[seriesID]
+					warnings.AddToContext(annotations.NewBadBucketLabelWarning(metricName, outputSeries.bucketLabelValue, posrange.PositionRange{}), ctx)
+				}
+				continue
+			}
+
+			outputSeriesID := outputSeries.outputID
+			bucket := promql.Bucket{
+				UpperBound: outputSeries.upperBound,
+				Count:      vector.Samples[i],
+			}
+			o.seriesBuckets[outputSeriesID] = append(o.seriesBuckets[outputSeriesID], bucket)
+		}
+
+		buf[n].Reset(vector.T)
+		for i, seriesID := range vector.HistogramIDs {
+			outputSeriesID := o.outputIndex[seriesID].outputID
+			// We need to check if there is a conventional histogram mapped to this output series ID.
+			// If that is the case, it means we have mixed data types for a single step and this behavior is undefined.
+			// In that case, we reset the conventional buckets to avoid emitting a sample.
+			if len(o.seriesBuckets[outputSeriesID]) == 0 {
+				var annos annotations.Annotations
+				var v float64
+				switch o.funcName {
+				case "histogram_quantile":
+					v, annos = promql.HistogramQuantile(o.scalar1Points[stepIndex], vector.Histograms[i], o.inputSeriesNames[seriesID], posrange.PositionRange{})
+					buf[n].AppendSample(uint64(outputSeriesID), v)
+				case "histogram_fraction":
+					v, annos = promql.HistogramFraction(o.scalar1Points[stepIndex], o.scalar2Points[stepIndex], vector.Histograms[i], o.inputSeriesNames[seriesID], posrange.PositionRange{})
+					buf[n].AppendSample(uint64(outputSeriesID), v)
+				}
+				warnings.MergeToContext(annos, ctx)
+			} else {
+				warnings.AddToContext(annotations.NewMixedClassicNativeHistogramsWarning(o.inputSeriesNames[seriesID], posrange.PositionRange{}), ctx)
+				o.seriesBuckets[outputSeriesID] = o.seriesBuckets[outputSeriesID][:0]
+			}
+		}
+
+		for i, stepBuckets := range o.seriesBuckets {
+			// It could be zero if multiple input series map to the same output series ID.
+			if len(stepBuckets) == 0 {
+				continue
+			}
+			// If we are after how many scalar points we have then it needs to be NaN.
+			if stepIndex >= len(o.scalar1Points) {
+				buf[n].AppendSample(uint64(i), math.NaN())
+				continue
+			}
+			switch o.funcName {
+			case "histogram_quantile":
+				// histogram_quantile needs at least 2 buckets.
+				if len(stepBuckets) == 1 {
+					buf[n].AppendSample(uint64(i), math.NaN())
+					continue
+				}
+				v, forcedMonotonicity, _ := promql.BucketQuantile(o.scalar1Points[stepIndex], stepBuckets)
+				buf[n].AppendSample(uint64(i), v)
+				if forcedMonotonicity {
+					warnings.AddToContext(annotations.NewHistogramQuantileForcedMonotonicityInfo(o.inputSeriesNames[i], posrange.PositionRange{}), ctx)
+				}
+			case "histogram_fraction":
+				// BucketFraction handles single bucket and other edge cases properly.
+				v := promql.BucketFraction(o.scalar1Points[stepIndex], o.scalar2Points[stepIndex], stepBuckets)
+				buf[n].AppendSample(uint64(i), v)
+			}
+		}
+		n++
+	}
+
+	return n, nil
+}
+
+func (o *histogramOperator) loadSeries(ctx context.Context) error {
+
+	o.vectorBuf = make([]model.StepVector, o.stepsBatch)
+	o.scalar1Buf = make([]model.StepVector, o.stepsBatch)
+	if o.scalar2Op != nil {
+		o.scalar2Buf = make([]model.StepVector, o.stepsBatch)
+	}
+
+	series, err := o.vectorOp.Series(ctx)
+	if err != nil {
+		return err
+	}
+
+	var (
+		hashBuf      = make([]byte, 0, 256)
+		hasher       = xxhash.New()
+		seriesHashes = make(map[uint64]int, len(series))
+	)
+
+	o.series = make([]labels.Labels, 0)
+	o.inputSeriesNames = make([]string, len(series))
+	o.outputIndex = make([]*histogramSeries, len(series))
+	b := labels.ScratchBuilder{}
+	for i, s := range series {
+		hasBucketValue := true
+		lbls, bucketLabel := extlabels.DropBucketLabel(s, b)
+		value, err := strconv.ParseFloat(bucketLabel.Value, 64)
+		if err != nil {
+			hasBucketValue = false
+		}
+
+		hasher.Reset()
+		hashBuf = lbls.Bytes(hashBuf)
+		if _, err := hasher.Write(hashBuf); err != nil {
+			return err
+		}
+
+		// We check for duplicate series after dropped labels when
+		// showing the result of the query. Series that are equal after
+		// dropping name should not hash to the same bucket here.
+		lbls = extlabels.DropReserved(lbls, b)
+
+		seriesHash := hasher.Sum64()
+		seriesID, ok := seriesHashes[seriesHash]
+		if !ok {
+			o.series = append(o.series, lbls)
+			seriesID = len(o.series) - 1
+			seriesHashes[seriesHash] = seriesID
+		}
+
+		o.inputSeriesNames[i] = s.Get(labels.MetricName)
+		o.outputIndex[i] = &histogramSeries{
+			outputID:         seriesID,
+			upperBound:       value,
+			hasBucketValue:   hasBucketValue,
+			bucketLabelValue: bucketLabel.Value,
+		}
+	}
+	o.seriesBuckets = make([]promql.Buckets, len(o.series))
+	o.badBucketWarned = make(map[uint64]bool)
+	return nil
+}
+
+func (o *histogramOperator) resetBuckets() {
+	for i := range o.seriesBuckets {
+		o.seriesBuckets[i] = o.seriesBuckets[i][:0]
+	}
+}
